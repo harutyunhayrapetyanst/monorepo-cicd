8d6019ed4c9a6743214e2c77824e038c58dd6611
Harutyun Hayrapetyan <hhayrapetyan@servicetitan.com>
2023-01-24 13:48:35 +0400

    fix(date-logic): remove log

--- packages/date-logic/index.js
+++ packages/date-logic/index.js
@@ -2,6 +2,6 @@ module.exports = function getDate(
     locale = 'en-US',
     options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'}
 ) {
-    console.log('create date')
+    console.log('create date');
     return new Date().toLocaleDateString(locale, options);
 };
