cba44c61f1d684537923160dea46dd30f670aaac
Harutyun Hayrapetyan <hhayrapetyan@servicetitan.com>
2023-01-24 21:01:04 +0400

    fix(date-renderer): remove log

--- packages/date-renderer/index.js
+++ packages/date-renderer/index.js
@@ -3,5 +3,4 @@ const getDate = require('@harutyunhayrapetyanst/date-logic');
 (() => {
     console.log(`Date: ${getDate()}`);
     console.log(`Date: ${getDate()}`);
-    console.log(`Date: ${getDate()}`);
 })();
