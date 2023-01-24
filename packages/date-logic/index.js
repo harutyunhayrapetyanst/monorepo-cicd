21041035356df41bd374ac5525616e1c9ddf3d7e
Harutyun Hayrapetyan <51323374+harutyunhayrapetyanst@users.noreply.github.com>
2023-01-24 21:15:27 +0400

    Br (#8)
    * fix(root): fix
    
    * fix(date-renderer): fix
    
    * fix(date-logic): fixxx

--- packages/date-logic/index.js
+++ packages/date-logic/index.js
@@ -3,5 +3,6 @@ module.exports = function getDate(
     options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'}
 ) {
     console.log('create date');
+    console.log('create date');
     return new Date().toLocaleDateString(locale, options);
 };
